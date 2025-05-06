bcb810fdcdc0b46eb59fe12047ff3276111ef0f6
ehtishamnazir1234@gmail.com <ehtishamnazir1234@mail.com>
2025-05-06 20:13:06 +0500

    pic updated

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePic from"../assets/image.png"
+import profilePic from"../assets/image2.jpg"
 import { useTheme } from "../context/ThemeContext";
 import { getThemeClasses } from "../utils/themeClasses";
 
